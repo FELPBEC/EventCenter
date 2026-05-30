2bf94326d6f812d1824f7885e9009240da7a189a
Julian <jm8597960@gmail.com>
2026-05-30 11:26:04 -0500

    <Jm1407db>/<MODIFY>/<Se implementaron fuentes y configuracion en app para mostrar ventana en maxima pantalla, y un ejemplo en fxml de como se veria>

--- event_center_project/src/main/java/co/edu/uptc/View/App.java
+++ event_center_project/src/main/java/co/edu/uptc/View/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -14,19 +15,31 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
         try {
-            scene = new Scene(loadFXML("mainView"), 800, 600);
+            Font.loadFont(App.class.getResourceAsStream("/co/edu/uptc/fonts/Pacifico-Regular.ttf"), 10);
+            Font.loadFont(App.class.getResourceAsStream("/co/edu/uptc/fonts/IBMPlexMono-Regular.ttf"), 10);
+            Font.loadFont(App.class.getResourceAsStream("/co/edu/uptc/fonts/Caveat-Regular.ttf"), 10);
+
+        
+            scene = new Scene(loadFXML("mainView"));
             
             stage.setTitle("Centro de Eventos Elite - Sistema de Gestión");
             stage.setScene(scene);
-            stage.setResizable(false);
+            
+            stage.setResizable(true);  
+            stage.setMaximized(true); 
+            
             stage.show();
             
         } catch (IOException e) {
             System.err.println("Error crítico: No se pudo cargar el FXML inicial 'mainView'.");
             e.printStackTrace();
+        } catch (NullPointerException e) {
+            System.err.println("Error de carga: Asegúrate de que las fuentes estén en /co/edu/uptc/fonts/.");
+            e.printStackTrace();
         }
     }
 
+    // Este método lo usaremos más adelante para cambiar de ventana sin perder el tamaño maximizado
     public static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
